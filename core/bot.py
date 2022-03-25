c4b1fda99c9ea798c007490f6f31b9c149834dde
remimd <remi.mandart@gmail.com>
2022-03-25 14:06:15 +0100

    Base

--- core/bot.py
+++ core/bot.py
@@ -1,14 +1,17 @@
 from discord import Client
+from discord_slash import SlashCommand
 
 import settings
 from services import logs
 
 
 class Bot(Client):
+    _slash: SlashCommand
     _token: str
 
     def __init__(self, **options):
         Client.__init__(self, **options)
+        self._slash = SlashCommand(self)
         self._token = settings.TOKEN
 
     def run(self, *args, **kwargs):
