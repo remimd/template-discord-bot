c7368affaf5118c28697b266e25ba223845b1ee3
remimd <remi.mandart@gmail.com>
2022-05-01 15:40:09 +0200

    feat: Update Django Commands

---
+++ core/management/commands/runserver.py
@@ -0,0 +1,43 @@
+import uvicorn
+from django.core.management import CommandParser
+from django.core.management.commands.runserver import Command as BaseRunServerCommand
+
+from api import application
+from core.discord.bot import Bot
+from services import logs
+
+
+class Command(BaseRunServerCommand):
+    default_port = 8000
+
+    def add_arguments(self, parser: CommandParser):
+        parser.add_argument(
+            "-p",
+            "--port",
+            default=self.default_port,
+            type=int,
+            help="change port",
+            dest="port",
+        )
+        parser.add_argument(
+            "-l",
+            "--logs",
+            action="store_true",
+            help="save custom logs",
+            dest="save_logs",
+        )
+
+    def handle(self, *args, **options):
+        @application.after_start
+        async def start_bot(_):
+            bot = Bot.get_instance()
+            bot.run_in_event_loop()
+
+        uvicorn_options = {
+            "port": options["port"] or self.default_port,
+        }
+
+        uvicorn.run(application, **uvicorn_options)
+
+        if options["save_logs"]:
+            logs.save()
--- main.py
+++ main.py
@@ -1,37 +1,12 @@
 import os
-from argparse import ArgumentParser
-from typing import Callable
+import sys
 
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-    command, kwargs = parse_arguments()
-    try:
-        command(**kwargs)
-    except TypeError:
-        command()
+    from django.core.management import execute_from_command_line
 
-
-def parse_arguments() -> tuple[Callable, dict]:
-    from core.management.manager import Manager
-
-    manager = Manager.get_instance()
-    parser = ArgumentParser()
-
-    command_keyword = "command"
-    parser.add_argument(command_keyword, choices=manager.commands_list)
-    parser.add_argument(
-        "-p", "--port", default=8000, type=int, help="change port", dest="port"
-    )
-    parser.add_argument(
-        "-l", "--logs", action="store_true", help="save custom logs", dest="save_logs"
-    )
-
-    kwargs = parser.parse_args().__dict__
-    command_name = kwargs.pop(command_keyword)
-    command = manager.get_command(command_name)
-
-    return command, kwargs
+    execute_from_command_line(argv=sys.argv)
 
 
 if __name__ == "__main__":
