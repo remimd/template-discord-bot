9a1b77ab49ac3b38c4fd1061b74fc3f60f42acbf
remimd <remi.mandart@gmail.com>
2022-05-10 17:58:36 +0200

    Feat: Update Webhook Service

--- services/discord/webhook.py
+++ services/discord/webhook.py
@@ -1,4 +1,7 @@
-from blacksheep.client import ClientSession
+import asyncio
+from typing import Iterable
+
+from aiohttp import ClientSession
 from discord import AsyncWebhookAdapter, Webhook
 
 
@@ -6,3 +9,7 @@ async def send(webhook_url: str, **kwargs):
     async with ClientSession() as session:
         webhook = Webhook.from_url(webhook_url, adapter=AsyncWebhookAdapter(session))
         await webhook.send(**kwargs)
+
+
+async def send_to_multiple(webhooks_urls: Iterable[str], **kwargs):
+    await asyncio.gather(send(url, **kwargs) for url in webhooks_urls)
