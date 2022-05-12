fa172c3f1ddeca6796c3643e3add44d96a31e4f9
remimd <remi.mandart@gmail.com>
2022-05-12 14:15:17 +0200

    Feat: Little thing

--- services/utils/date.py
+++ services/utils/date.py
@@ -4,10 +4,10 @@ from common.static_class import StaticClass
 
 
 class Format(StaticClass):
-    LOG = "%b-%d-%Y %H:%M:%S"
-    FILE = "%Y-%m-%d_%H-%M-%S"
-    MESSAGE = "%B %d, %Y ~ %H:%M:%S"
     DROP = "%B %d, %Y ~ %H:%M:%S.%f"
+    FILE = "%Y-%m-%d_%H-%M-%S"
+    LOG = "%b-%d-%Y %H:%M:%S"
+    MESSAGE = "%B %d, %Y ~ %H:%M:%S"
 
 
 def now(format: str = Format.LOG) -> str:
