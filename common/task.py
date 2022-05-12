48f4aa396171b0bed4f6b04f3968569df3b3fb19
remimd <remi.mandart@gmail.com>
2022-05-12 13:57:57 +0200

    Feat: Task V2

--- common/task.py
+++ common/task.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import asyncio
 from abc import ABC, abstractmethod
 from threading import Thread
@@ -5,22 +7,52 @@ from threading import Thread
 from services import logs
 
 
-class ParallelTask(Thread, ABC):
+_tasks_list: list[AbstractTask] = []
+
+
+def get_tasks_list() -> list[AbstractTask]:
+    return _tasks_list
+
+
+class AbstractTask(ABC):
+    disable: bool
+    name: str
+
+    def __new__(cls, *args, **kwargs):
+        instance = super(AbstractTask, cls).__new__(cls)
+        _tasks_list.append(instance)
+        return instance
+
+    def __init__(self, name: str = None):
+        self.disable = False
+        self.name = name or self.__class__.__name__
+
+    def __str__(self) -> str:
+        return self.name
+
+    @abstractmethod
+    def start(self):
+        raise NotImplementedError
+
+
+class ParallelTask(Thread, AbstractTask, ABC):
     seconds: float
 
     def __init__(
-        self, days: float = 0, hours: float = 0, minutes: float = 0, seconds: float = 0
+        self,
+        name: str = None,
+        days: float = 0,
+        hours: float = 0,
+        minutes: float = 0,
+        seconds: float = 0,
     ):
-        name = self.__class__.__name__
-        super(ParallelTask, self).__init__(name=name, daemon=True)
+        Thread.__init__(self, daemon=True)
+        AbstractTask.__init__(self, name=name)
         self.seconds = days * 86400 + hours * 3600 + minutes * 60 + seconds
 
-    def clone(self):
-        return self.__class__()
-
     def start(self):
-        super(ParallelTask, self).start()
-        logs.ok(f'Parallel Task: "{self.name}" ready')
+        Thread.start(self)
+        logs.ok(f'Parallel Task: "{self}" ready')
 
     def run(self):
         coroutine = self._loop()
@@ -28,9 +60,12 @@ class ParallelTask(Thread, ABC):
 
     async def _loop(self):
         while True:
+            if self.disable:
+                continue
+
             await self.execute()
             await asyncio.sleep(self.seconds)
 
     @abstractmethod
     async def execute(self):
-        pass
+        raise NotImplementedError
