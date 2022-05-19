ca6fc0c66e21ccfc4d428f8e1bf0dacbe6ba327f
remimd <remi.mandart@gmail.com>
2022-05-19 14:21:21 +0200

    Feat: Singleton thread-safe

--- common/singleton.py
+++ common/singleton.py
@@ -1,16 +1,20 @@
 from __future__ import annotations
 
 from abc import ABC
+from threading import Lock
 from typing import Optional
+
+
+_lock = Lock()
 
 
 class Singleton(ABC):
     _instance: Optional[Singleton] = None
 
     def __new__(cls, *args, **kwargs):
-        if cls._instance:
-            raise RuntimeError(f"{cls.__name__} can only be built once.")
-        else:
+        with _lock:
+            if cls._instance:
+                raise RuntimeError(f"{cls.__name__} can only be built once.")
             cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
 
