4734a6d4713a860de7dd4d77980b9ffe77c93adb
remimd <remi.mandart@gmail.com>
2022-07-11 22:26:01 +0200

    Update: Message Service

--- services/discord/message_service.py
+++ services/discord/message_service.py
@@ -1,2 +1,6 @@
-def mention(discord_id: int) -> str:
-    return f"<@&{discord_id}>"
+def mention_role(discord_role_id: int) -> str:
+    return f"<@&{discord_role_id}>"
+
+
+def mention_user(discord_user_id: int) -> str:
+    return f"<@{discord_user_id}>"
