d9d5862d4a197627ff0f353206fbd4c8dee43ebb
remimd <remi.mandart@gmail.com>
2022-07-13 16:06:29 +0200

    Feat: Crud + Use Serpy Serializer

--- common/api/crud.py
+++ common/api/crud.py
@@ -1,17 +1,16 @@
-from typing import Any, Type
+from typing import Type
 
-from blacksheep import Response, bad_request, created, not_found, ok
+from blacksheep import Response, bad_request, created, not_found, ok, pretty_json
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Model
-
-from common.api.serializer import Serializer
+from serpy import Serializer
 
 
 class Crud:
     model: Type[Model]
-    serializer: Serializer
+    serializer: Type[Serializer]
 
-    def __init__(self, model: Type[Model], serializer: Serializer):
+    def __init__(self, model: Type[Model], serializer: Type[Serializer]):
         self.model = model
         self.serializer = serializer
 
@@ -19,33 +18,33 @@ class Crud:
     def model_name(self) -> str:
         return self.model.__name__
 
-    def get_list(self) -> list[object]:
+    def get_list(self) -> Response:
         object_list = self.model.objects.all()
-        return [self.serializer.serialize(o) for o in object_list]
+        return pretty_json(self.serializer(object_list, many=True).data)
 
-    def get_by_pk(self, pk: int | str) -> object | Response:
+    def get_by_pk(self, primary_key) -> Response:
         try:
-            o = self.model.objects.get(pk=pk)
-            return self.serializer.serialize(o)
+            o = self.model.objects.get(pk=primary_key)
+            return pretty_json(self.serializer(o).data)
         except ObjectDoesNotExist:
             return not_found()
 
-    def create(self, values: dict[str, Any]) -> Response:
+    def create(self, **kwargs) -> Response:
         try:
-            o = self.model.objects.create(**values)
+            o = self.model.objects.create(**kwargs)
             o.save()
         except TypeError as exc:
             return bad_request(str(exc))
 
         return created()
 
-    def edit(self, pk: int | str, values: dict[str, Any]) -> Response:
+    def edit(self, primary_key, **kwargs) -> Response:
         try:
-            o = self.model.objects.get(pk=pk)
+            o = self.model.objects.get(pk=primary_key)
         except ObjectDoesNotExist:
             return not_found()
 
-        for key, value in values.items():
+        for key, value in kwargs.items():
             if not hasattr(o, key):
                 return bad_request(f"{self.model_name} doesn't have attribute '{key}'.")
 
@@ -54,9 +53,9 @@ class Crud:
         o.save()
         return ok()
 
-    def delete(self, pk: int | str) -> Response:
+    def delete(self, primary_key) -> Response:
         try:
-            o = self.model.objects.get(pk=pk)
+            o = self.model.objects.get(pk=primary_key)
         except ObjectDoesNotExist:
             return not_found()
 
